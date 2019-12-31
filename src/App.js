78c3c8dc770bbeb756303e45ccca2b40d90dccff
yangmengru <15932695730@163.com>
2019-12-31 11:17:38 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { observer } from 'mobx-react';
+@observer
 function App() {
   return (
     <div className="App">
